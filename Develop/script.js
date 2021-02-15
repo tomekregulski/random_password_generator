c8b3b94fd983447b5b653e2dc2feda9b03df618f
Tomek Regulski <tomek.regulski@gmail.com>
2021-02-15 10:57:38 -0500

    successfully connected new code to starter code. app has passed several tests and seems to be working in a way that asaisfies all requirements

--- Develop/script.js
+++ Develop/script.js
@@ -5,52 +5,59 @@ var upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numSet = "0123456789";
 var specialSet = "!@#$%^&*";
 
+
 // Write password to the #password input
+function generatePassword() {
+  var password = [];
+  var passwordLength = prompt("How many characters would you like your password to be? Please eanter a number between 8 and 128");
+  var lowCase = confirm("Would you like your password to include lowercase letters?");
+    console.log(lowCase);
+  var upCase = confirm("Would you like your password to include uppercase letters?");
+    console.log(upCase);
+  var numbers = confirm("Would you like your password to include numbers?");
+    console.log(numbers);
+  var specialChars = confirm("Would you like your password to include special characters?");
+    console.log(specialChars);
+  var passwordSet = "";
+
+    if (lowCase) { 
+      passwordSet = lowerSet;
+      // console.log(passwordSet); 
+    }
+
+    if (upCase) {
+      passwordSet = passwordSet + upperSet;
+      // console.log(passwordSet);
+    }
+
+    if (numbers) {
+      passwordSet = passwordSet + numSet;
+      // console.log(passwordSet);
+    }
+
+    if (specialChars) {
+      passwordSet = passwordSet + specialSet;
+    // console.log(passwordSet);
+    }
+
+    console.log(passwordLength);
+    for ( var i = 0; i < passwordLength; i++ ) {
+      password += passwordSet[Math.floor(Math.random() * passwordSet.length)];
+  }
+
+  return password;
+
+};
+
 
 function writePassword() {
-   // var password = generatePassword(); Questionable starter code
-    var newPassword = [];
-    var passwordLength = prompt("How many characters would you like your password to be? Please eanter a number between 8 and 128");
-    var lowCase = confirm("Would you like your password to include lowercase letters?");
-      console.log(lowCase);
-    var upCase = confirm("Would you like your password to include uppercase letters?");
-      console.log(upCase);
-    var numbers = confirm("Would you like your password to include numbers?");
-      console.log(numbers);
-    var specialChars = confirm("Would you like your password to include special characters?");
-      console.log(specialChars);
-    var passwordSet = "";
+  var password = generatePassword()
   
-      if (lowCase) { 
-        passwordSet = lowerSet;
-        // console.log(passwordSet); 
-      }
-  
-      if (upCase) {
-        passwordSet = passwordSet + upperSet;
-        // console.log(passwordSet);
-      }
-  
-      if (numbers) {
-        passwordSet = passwordSet + numSet;
-        // console.log(passwordSet);
-      }
-  
-      if (specialChars) {
-        passwordSet = passwordSet + specialSet;
-      // console.log(passwordSet);
-      }
+  var passwordText = document.querySelector("#password");
 
-      console.log(passwordLength);
-      for ( var i = 0; i < passwordLength; i++ ) {
-        newPassword += passwordSet[Math.floor(Math.random() * passwordSet.length)];
-    }
-  
-    console.log(newPassword);
-    var passwordText = document.querySelector("#password");
-      passwordText.value = newPassword;
-  };
+  passwordText.value = password;
 
+}
 
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
