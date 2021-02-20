461cd1e42902a33979a0492fd92db41d864ed71d
Tomek Regulski <tomek.regulski@gmail.com>
2021-02-19 19:28:17 -0500

    cleaned up indentation on JS

--- Develop/script.js
+++ Develop/script.js
@@ -10,55 +10,50 @@ var specialSet = "!()?[]`~;:!@#$%^&*+=";
 function generatePassword() {
   var password = [];
   var passwordLength = prompt("How many characters would you like your password to be? Please enter a number between 8 and 128");
-  if (passwordLength === null) {
-    return ("");
-  }
-  if (passwordLength < 8 || passwordLength > 128 || isNaN(parseInt(passwordLength))) {
-    alert("Please choose a number between 8 and 128 characters.")
-    var tryAgain = confirm("Would you like to try again?");
-    if (tryAgain) {
-      generatePassword();
-    } else {
+    if (passwordLength === null) {
       return ("");
     }
-  } else {
-    var lowCase = confirm("Would you like your password to include lowercase letters?");
-      console.log(lowCase);
-    var upCase = confirm("Would you like your password to include uppercase letters?");
-      console.log(upCase);
-    var numbers = confirm("Would you like your password to include numbers?");
-      console.log(numbers);
-    var specialChars = confirm("Would you like your password to include special characters?");
-      console.log(specialChars);
-    var passwordSet = "";
-
-      if (lowCase) { 
-        passwordSet = lowerSet;
-      }
-      if (upCase) {
-        passwordSet = passwordSet + upperSet;
-      }
-      if (numbers) {
-        passwordSet = passwordSet + numSet;
-      }
-      if (specialChars) {
-        passwordSet = passwordSet + specialSet;
-      }
-
-      if (passwordSet === "" ) {
-        alert("You must select at least one character set.")
-        return ("");
+    if (passwordLength < 8 || passwordLength > 128 || isNaN(parseInt(passwordLength))) {
+      alert("Please choose a number between 8 and 128 characters.")
+      var tryAgain = confirm("Would you like to try again?");
+      if (tryAgain) {
+        generatePassword();
       } else {
-      console.log(passwordLength);
-      for ( var i = 0; i < passwordLength; i++ ) {
-        password += passwordSet[Math.floor(Math.random() * passwordSet.length)];
-    }
-    // return password;
-    var passwordText = document.querySelector("#password");
+        return ("");
+      }
+    } else {
+      var lowCase = confirm("Would you like your password to include lowercase letters?");
+      var upCase = confirm("Would you like your password to include uppercase letters?");
+      var numbers = confirm("Would you like your password to include numbers?");
+      var specialChars = confirm("Would you like your password to include special characters?");
+      var passwordSet = "";
 
-    passwordText.value = password;
- }
-}
+        if (lowCase) { 
+          passwordSet = lowerSet;
+        }
+        if (upCase) {
+          passwordSet = passwordSet + upperSet;
+        }
+        if (numbers) {
+          passwordSet = passwordSet + numSet;
+        }
+        if (specialChars) {
+          passwordSet = passwordSet + specialSet;
+        }
+
+        if (passwordSet === "" ) {
+          alert("You must select at least one character set.")
+          return ("");
+        } else {
+        for ( var i = 0; i < passwordLength; i++ ) {
+          password += passwordSet[Math.floor(Math.random() * passwordSet.length)];
+        }
+        // return password;
+        var passwordText = document.querySelector("#password");
+
+        passwordText.value = password;
+      }
+    }
 };
 
 // function writePassword() {
