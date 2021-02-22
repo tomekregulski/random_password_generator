d750c83029f83f02627d4c359fbd616f40875737
Tomek Regulski <tomek.regulski@gmail.com>
2021-02-22 09:04:05 -0500

    updated JS with comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Declare global variables
 var generateBtn = document.querySelector("#generate");
 var lowerSet = "abcdefghijklmnopqrstuvwxyz";
 var upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -9,10 +9,12 @@ var specialSet = "!()?[]`~;:!@#$%^&*+=";
 // Write password to the #password input
 function generatePassword() {
   var password = [];
+// Prompt user to input parameters for password generation
   var passwordLength = prompt("How many characters would you like your password to be? Please enter a number between 8 and 128");
     if (passwordLength === null) {
       return ("");
     }
+// catch instances of invalid entry
     if (passwordLength < 8 || passwordLength > 128 || isNaN(parseInt(passwordLength))) {
       alert("Please choose a number between 8 and 128 characters.")
       var tryAgain = confirm("Would you like to try again?");
@@ -26,6 +28,7 @@ function generatePassword() {
       var upCase = confirm("Would you like your password to include uppercase letters?");
       var numbers = confirm("Would you like your password to include numbers?");
       var specialChars = confirm("Would you like your password to include special characters?");
+// concat character strings based on parameters set by user
       var passwordSet = "";
 
         if (lowCase) { 
@@ -40,11 +43,12 @@ function generatePassword() {
         if (specialChars) {
           passwordSet = passwordSet + specialSet;
         }
-
+// Ensure that user has selected at least one character set
         if (passwordSet === "" ) {
           alert("You must select at least one character set.")
           return ("");
         } else {
+// Loop through final character set and select random characters, with number of iterations set to the user's desired length for the password.
         for ( var i = 0; i < passwordLength; i++ ) {
           password += passwordSet[Math.floor(Math.random() * passwordSet.length)];
         }
@@ -56,12 +60,4 @@ function generatePassword() {
     }
 };
 
-// function writePassword() {
-//   var password = generatePassword()
-  
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
